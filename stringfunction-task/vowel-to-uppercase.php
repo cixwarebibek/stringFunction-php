a4bf331d6f033ec8975a0ad55e5b22a56edcbef8
Bibek Bhandari <Cixwarebibek@gmail.com>
2020-03-03 14:21:22 +0545

    put color and minimize the code

--- stringfunction-task/vowel-to-uppercase.php
+++ stringfunction-task/vowel-to-uppercase.php
@@ -1,15 +1,29 @@
 <html>
+<body>
 <?php
- $str="Lorem ipsum dolor sit amet, esse consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla esse pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt esse mollit anim id est laborum.";
- $vowelsA = str_replace("a","A","$str");
-
- $vowelsE = str_replace("e","E","$vowelsA");
- $vowelsI = str_replace("i","I","$vowelsE");
- $vowelsO = str_replace("o","O","$vowelsI");
- $vowelsU = str_replace("u","U","$vowelsO");
-
-
- echo $vowelsU;
-
+        $str="Lorem ipsum dolor sit amet, esse consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla esse pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt esse mollit anim id est laborum.";
+        $vowels=array("a","e","i","o","u");
+        $replaceval=array('<span class="C1"> A </span>','<span class="C2">E</span>','<span class="C3">I</span>','<span class="C4">O</span>','<span class="C5">U</span>');
+        echo str_replace($vowels,$replaceval,$str);
 ?>
+
+<style>
+     .C1{
+         color:red;
+     }
+     .C2{
+         color:green;
+     }
+     .C3{
+         color:blue;
+     }
+     .C4{
+         color:orange;
+     }
+     .C5{
+         color:yellow;
+     }
+
+</style>
+</body>
 </html>
